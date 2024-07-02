06377011cb4382e8c826eaafcbb449088258f257
Pritam Sur <sur.pritam9878@gmail.com>
2024-07-01 23:00:42 -0400

    updated query for getRoles and getEmployees

--- queries.js
+++ queries.js
@@ -6,12 +6,16 @@ async function getDepartments() {
 }
 
 async function getRoles() {
-  const res = await pool.query('SELECT * FROM role');
+  const res = await pool.query('SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id');
   return res.rows;
 }
 
 async function getEmployees() {
-  const res = await pool.query('SELECT * FROM employee');
+  const res = await pool.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, 
+                                (SELECT CONCAT(manager.first_name, ' ', manager.last_name) FROM employee AS manager WHERE manager.id = employee.manager_id) AS manager 
+                                FROM employee 
+                                LEFT JOIN role ON employee.role_id = role.id 
+                                LEFT JOIN department ON role.department_id = department.id`);
   return res.rows;
 }
 

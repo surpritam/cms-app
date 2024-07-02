482954e9a2b662530cf6c9b25cc03931e78aa530
Pritam Sur <sur.pritam9878@gmail.com>
2024-07-01 23:10:10 -0400

    Add new department functionality

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
 const inquirer = require('inquirer');
-const { getDepartments, getRoles, getEmployees } = require('./queries');
+const { getDepartments, getRoles, getEmployees, addDepartment } = require('./queries');
 
 async function mainMenu() {
-  const answer = await inquirer.prompt({
-    name: 'action',
-    type: 'list',
-    message: 'What would you like to do?',
-    choices: ['View Departments', 'View Roles', 'View Employees', 'Exit'],
-  });
+    const answer = await inquirer.prompt({
+        name: 'action',
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: ['View Departments', 'View Roles', 'View Employees', 'Add Department', 'Exit'],
+    });
 
-  switch (answer.action) {
-    case 'View Departments':
-      const departments = await getDepartments();
-      console.table(departments);
-      break;
-    case 'View Roles':
-      const roles = await getRoles();
-      console.table(roles);
-      break;
-    case 'View Employees':
-      const employees = await getEmployees();
-      console.table(employees);
-      break;
-    case 'Exit':
-      process.exit();
-  }
+    switch (answer.action) {
+        case 'View Departments':
+            const departments = await getDepartments();
+            console.table(departments);
+            break;
+        case 'View Roles':
+            const roles = await getRoles();
+            console.table(roles);
+            break;
+        case 'View Employees':
+            const employees = await getEmployees();
+            console.table(employees);
+            break;
+        case 'Add Department':
+            await promptAddDepartment();
+            break;
+        case 'Exit':
+            process.exit();
+    }
 
-  mainMenu();
+    mainMenu();
 }
+
+async function promptAddDepartment() {
+    const answer = await inquirer.prompt({
+      name: 'name',
+      type: 'input',
+      message: 'Enter the name of the department:',
+    });
+    const newDepartment = await addDepartment(answer.name);
+    console.log(`Added department: ${newDepartment.name}`);
+  }
 
 mainMenu();
--- queries.js
+++ queries.js
@@ -1,22 +1,27 @@
 const pool = require('./db');
 
 async function getDepartments() {
-  const res = await pool.query('SELECT * FROM department');
-  return res.rows;
+    const res = await pool.query('SELECT * FROM department');
+    return res.rows;
 }
 
 async function getRoles() {
-  const res = await pool.query('SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id');
-  return res.rows;
+    const res = await pool.query('SELECT role.id, role.title, role.salary, department.name AS department FROM role LEFT JOIN department ON role.department_id = department.id');
+    return res.rows;
 }
 
 async function getEmployees() {
-  const res = await pool.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, 
+    const res = await pool.query(`SELECT employee.id, employee.first_name, employee.last_name, role.title AS job_title, department.name AS department, role.salary, 
                                 (SELECT CONCAT(manager.first_name, ' ', manager.last_name) FROM employee AS manager WHERE manager.id = employee.manager_id) AS manager 
                                 FROM employee 
                                 LEFT JOIN role ON employee.role_id = role.id 
                                 LEFT JOIN department ON role.department_id = department.id`);
-  return res.rows;
+    return res.rows;
 }
 
-module.exports = { getDepartments, getRoles, getEmployees };
+async function addDepartment(name) {
+    const res = await pool.query('INSERT INTO department (name) VALUES ($1) RETURNING *', [name]);
+    return res.rows[0];
+}
+
+module.exports = { getDepartments, getRoles, getEmployees, addDepartment };
